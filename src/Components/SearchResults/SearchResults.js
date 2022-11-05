8e6922b42c789dc9b57d7cebf92a7afed3c5d472
Dusty <dustyneubauer@gmail.com>
2022-11-04 20:03:35 -0500

    Initial Commit
    Initial commit for a react app using a spotify API

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,13 @@
+import './SearchResults.css';
+// import 'TrackList' from ;
+
+export class SearchResults extends React.Component {
+    render(){
+        returns(
+            <div className="SearchResults">
+  <h2>Results</h2>
+  {/* <TrackList /> */}
+</div>
+        )
+    }
+}
